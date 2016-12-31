a4f614b464fe732fb76d0394e2bfebe980da1a82
AJAYKUMAR <ajay14102.ec@rmkec.ac.in>
2016-12-31 08:50:38 +0530

    Update matrixindices.c

--- matrixindices.c
+++ matrixindices.c
@@ -7,13 +7,15 @@ int main(){
      scanf("%d",&k);
     printf("matrix");
     for(i=0;i<n;i++)
-      for(j=0;j<m;j++)
+    {
+        for(j=0;j<m;j++)
         {
           if(k==(i+j))
              a[i][j]=1;
           else
              a[i][j]=0;
         }
+    }
     for(i=0;i<n;i++)
       for(j=0;j<m;j++)
          printf("%d\t",a[i][j]);
