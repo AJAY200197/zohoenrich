876b38a4c921a41023e8a6f60e4249883e9adbc7
AJAYKUMAR <ajay14102.ec@rmkec.ac.in>
2017-02-03 17:03:31 +0530

    Create indexarray.c

---
+++ indexarray.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() 
+{
+	int a[10],b[10],key,i,n;
+	scanf("%d",&n);
+	printf("Input\nArray:");
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	printf("Number to Search:");
+	scanf("%d",&key);
+		for(i=0;i<n;i++)
+    {
+        if(a[i]==key)
+        {
+            b[i]=i;
+            printf("%d,",b[i]);
+        }
+    }
+	return 0;
+}
