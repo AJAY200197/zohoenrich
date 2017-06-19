279d325db029d549cc73704ebe6caf5f222b4840
AJAYKUMAR <ajay14102.ec@rmkec.ac.in>
2017-06-19 11:05:52 +0530

    Create split.java

---
+++ split.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		int l = a.length();
+		int i=0,c=0;
+		for( i =0;i<l;i++){
+		    if(a.charAt(i) == '@'){
+		        c++;
+		    }
+		}int t = c/2;
+		int o=0,v=0;
+		String[] b = a.split(" ");
+		for(i=0;i<c;i++)
+		{
+		String[] di = b[i].split("@");
+		int ei = Integer.parseInt(di[0]);
+		int fi = Integer.parseInt(di[1]);
+		o = o+ei;
+		v= v+fi;
+	   }
+	   float avg = o/f;
+	   System.out.printf(avg);
+    }
+}
